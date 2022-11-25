b6235faa5c0a19a59e9987b4cd74e314fb760830
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-25 19:04:29 -0300

    refactoring and more features

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 // DOM 
 // Document Object Model
+// Refatoração: mudar um código para deixá-lo mais entendível e performático, sem alterar suas funcionalidades
 
-let play = document.querySelector('.play')
-let pause =  document.querySelector('.pause')
+const buttonPlay = document.querySelector('.play')
+const buttonPause =  document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.sound-on')
+const buttonSoundOff = document.querySelector('.sound-off')
 
 // Event-driven (dirigido a evento)
 // Programação Imperativa (Programação que dá ordens, passo a passo do que precisa ser feito)
 // Callback (chamar de volta) ou seja, vai ficar guardado e vai executar devido a alguma ação
-play.addEventListener('click', function() {
-  play.classList.add('hide')
-  pause.classList.remove('hide')
+buttonPlay.addEventListener('click', function() {
+  buttonPlay.classList.add('hide')
+  buttonPause.classList.remove('hide')
+  buttonSet.classList.add('hide')
+  buttonStop.classList.remove('hide')
 })
 
-pause.addEventListener('click', function() {
-  pause.classList.add('hide')
-  play.classList.remove('hide')
+buttonPause.addEventListener('click', function() {
+  buttonPause.classList.add('hide')
+  buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click', function() {
+  buttonSoundOn.classList.remove('hide')
+  buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click', function() {
+  buttonSoundOn.classList.add('hide')
+  buttonSoundOff.classList.remove('hide')
 })
