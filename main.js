1dd247368364307f93a10040ee4ec6d20a34b3f6
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-25 18:45:26 -0300

    addEventListener ('click')

--- main.js
+++ main.js
@@ -2,8 +2,17 @@
 // Document Object Model
 
 let play = document.querySelector('.play')
+let pause =  document.querySelector('.pause')
 
-play.classList.add('hide')
+// Event-driven (dirigido a evento)
+// Programação Imperativa (Programação que dá ordens, passo a passo do que precisa ser feito)
+// Callback (chamar de volta) ou seja, vai ficar guardado e vai executar devido a alguma ação
+play.addEventListener('click', function() {
+  play.classList.add('hide')
+  pause.classList.remove('hide')
+})
 
-document.querySelector('.pause')
-.classList.remove('hide')
+pause.addEventListener('click', function() {
+  pause.classList.add('hide')
+  play.classList.remove('hide')
+})
