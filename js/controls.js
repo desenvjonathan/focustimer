35e3c41652f4f7883de1136fbfcbbf567efd23b4
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-26 17:18:00 -0300

    changing ES6 but not working

--- js/controls.js
+++ js/controls.js
@@ -1,7 +1,8 @@
-export function resetControls() { //programação imperativa (o que precisa ser feito)
+function resetControls() { //programação imperativa (o que precisa ser feito)
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 }
 
+export { resetControls }
--- js/timer.js
+++ js/timer.js
@@ -1,14 +1,14 @@
-export function updateTimerDisplay(minutes, seconds) {
+function updateTimerDisplay(minutes, seconds) {
   minutesDisplay.textContent = String(minutes).padStart(2,'0')
   secondsDisplay.textContent = String(seconds).padStart(2,'0')
 }
 
-export function resetTimer() {
+function resetTimer() {
   updateTimerDisplay(minutes, 0) 
   clearTimeout(timerTimeOut)
 }
 
-export function countdown() {
+function countdown() {
   timerTimeOut = setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
@@ -30,3 +30,6 @@ export function countdown() {
     countdown()
   }, 1000) // 1000 = é em milesegundos, ou seja, 1000ms = 1s
 }
+
+//named export
+export {}
