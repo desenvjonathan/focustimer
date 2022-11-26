71d5b616f0687405e000996bcc9bb7fdd1c2f199
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-25 21:25:01 -0300

    add countdown

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+function countdown() {
+  setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent)
+    if(seconds <= 0){
+      seconds = 60
+    }
+    secondsDisplay.textContent = seconds -1
+    countdown()
+  }, 1000) // 1000 = é em milesegundos, ou seja, 1000ms = 1s
+}
 
 // Event-driven (dirigido a evento)
 // Programação Imperativa (Programação que dá ordens, passo a passo do que precisa ser feito)
@@ -17,6 +30,8 @@ buttonPlay.addEventListener('click', function() {
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
 })
 
 buttonPause.addEventListener('click', function() {
@@ -39,4 +54,9 @@ buttonSoundOff.addEventListener('click', function() {
 buttonSoundOn.addEventListener('click', function() {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function() {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
 })
